62f7938cbf9b266a748f1b531f1bb30b8b695fd0
Phong Vu <phongvh2@topica.edu.vn>
2018-02-09 17:36:14 +0700

    change id to _id

--- resources/views/pages/table_contact_c3.blade.php
+++ resources/views/pages/table_contact_c3.blade.php
@@ -17,8 +17,8 @@
     </thead>
     <tbody>
         @foreach ($contacts as $item)
-        <tr id="contact-{{ $item->id }}">
-            <td><a href="javascript:void(0)" class="name" data-id="{{ $item->id }}">{{ $item->name }}</a></td>
+        <tr id="contact-{{ $item->_id }}">
+            <td><a href="javascript:void(0)" class="name" data-id="{{ $item->_id }}">{{ $item->name }}</a></td>
             <td>{{ $item->email }}</td>
             <td>{{ $item->phone }}</td>
             <td>{{ Date('d-m-Y H:i:s', $item->submit_time/1000) }}</td>
@@ -30,11 +30,11 @@
             <td>{{ $item->landing_page }}</td>
             <td>
                 {{--@permission('edit-review')--}}
-                <a href="javascript:void(0)" class="name btn btn-default btn-xs" data-id="{{ $item->id }}"><i
+                <a href="javascript:void(0)" class="name btn btn-default btn-xs" data-id="{{ $item->_id }}"><i
                             class='fa fa-eye'></i></a>
                 {{--<a data-toggle="modal" class='btn btn-xs btn-default'
                        data-target="#deleteModal"
-                       data-item-id="{{ $item->id }}"
+                       data-item-id="{{ $item->_id }}"
                        data-item-name="{{ $item->name }}"
                        data-original-title='Delete Row'><i
                         class='fa fa-times'></i></a>--}}
