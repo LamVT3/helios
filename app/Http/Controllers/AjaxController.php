7375c043bb812d5eda031011c023aa8f1fe2f3cf
LAPDELL-IT06L\Topica <thienlh@topica.edu.vn>
2018-05-15 19:38:25 +0700

    HEL-34
    - add the filter in time period

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -655,7 +655,7 @@ class AjaxController extends Controller
 
         $limit    = intval($request->length);
         $offset   = intval($request->start);
-        $contacts = $this->arrayToObject(array_slice($array, $offset, $limit));
+        $contacts = $query->skip($offset)->take($limit)->get();
 
         $data['contacts']   = $this->formatRecord($contacts);
         $data['total']      = count($total);
