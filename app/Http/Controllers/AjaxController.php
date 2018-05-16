0c14837cb3491929901d83a6f8021bbb3f5eaa3c
LAPDELL-IT06L\Topica <thienlh@topica.edu.vn>
2018-05-16 19:48:26 +0700

    HEL-36
    - add view the contacts was duplicated

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -738,6 +738,7 @@ class AjaxController extends Controller
             $contact['subcampaign_name']    = $contact['subcampaign_name'] ? $contact['subcampaign_name'] : "-";
             $contact['ad_name']             = $contact['ad_name'] ? $contact['ad_name'] : "-";
             $contact['landing_page']        = $contact['landing_page'] ? $contact['landing_page'] : "-";
+            $contact['duplicate_number']    = count(Contact::where('_id', '<>', $contact['_id'])->where('phone', $contact['phone'])->get());
         }
 
         return $contacts;
