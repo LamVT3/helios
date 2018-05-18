6739642f17026c633caf8dfdd4d0b8784c037d61
root <root@db2.novalocal>
2018-05-18 09:31:37 +0000

    user-edit role

--- resources/views/pages/user-edit.blade.php
+++ resources/views/pages/user-edit.blade.php
@@ -85,9 +85,9 @@
                                                     <select name="role">
                                                         <option value="Marketer" {{ old('role', isset($user->role) ? $user->role : "Manager") == "Marketer" ? 'selected' : '' }}>Marketer</option>
                                                         <option value="Manager" {{ old('role', isset($user->role) ? $user->role : "Manager") != "Marketer" ? 'selected' : '' }}>Manager</option>
-                                                        @if(auth()->user()->role == "Admin" && isset($user->role) && $user->role == 'Admin')
+                                                        @if(isset($user->role) && $user->role == 'Admin')
                                                             <option value="Admin" selected>Admin</option>
-                                                        @else
+                                                        @elseif(auth()->user()->role == "Admin")
                                                             <option value="Admin">Admin</option>
                                                         @endif
                                                     </select>
