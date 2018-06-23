659d4aebee975212dce87226c2498c62afd11ea2
Thanh Lam <lamvt@topica.edu.vn>
2018-06-23 15:17:21 +0700

    update filter for dif contacts

--- app/Http/Controllers/DiffContactsController.php
+++ app/Http/Controllers/DiffContactsController.php
@@ -30,17 +30,10 @@ class DiffContactsController extends Controller
         $config = Config::all();
         $page_size  = Config::getByKey('PAGE_SIZE');
 
-        $start_date = '2018-06-17';
-        $end_date = date("Y-m-d");;
+//        $start_date = '2018-06-17';
 
-        $helios_contacts    = Contact::where('submit_time', '>=', strtotime($start_date)*1000)
-            ->where('submit_time', '<', strtotime("tomorrow")*1000)
-            ->where('clevel', 'like', '%c3b%')
-            ->orderBy('submit_time', 'desc')->limit((int)$page_size)->get();
 
-        $mol_contacts       = $this->getMOLContacts($start_date, $end_date);
-
-        list($helios_diff, $mol_diff) = $this->diff_contact($helios_contacts, $mol_contacts);
+        list($helios_diff, $mol_diff) = $this->diff_contact();
 
         return view('pages.diff_contacts', compact(
             'page_title',
@@ -69,8 +62,6 @@ class DiffContactsController extends Controller
         $data     = @$response['data'];
 
         return $data;
-
-
     }
 
     private function callAPI($method, $url, $data){
@@ -105,35 +96,78 @@ class DiffContactsController extends Controller
         return $result;
     }
 
-    private function diff_contact($helios_contacts, $mol_contacts){
+    private function diff_contact(){
 
-        $arr_phone_helios   = array();
-        $arr_phone_mol      = array();
-
-        foreach($helios_contacts as $contact){
-            $arr_phone_helios[] = '0'.$contact->phone;
+        $start_date = Date('Y-m-d');
+        $end_date = Date('Y-m-d');
+        if(request('registered_date')){
+            $date_place = str_replace('-', ' ', request('registered_date'));
+            $date_arr = explode(' ', str_replace('/', '-', $date_place));
+            $start_date = Date('Y-m-d', strtotime($date_arr[0]));
+            $end_date = Date('Y-m-d', strtotime($date_arr[1]));
         }
 
-        foreach($mol_contacts as $contact){
-            $arr_phone_mol[] = $contact['phone'];
-        }
+        $helios_contacts    = Contact::where('submit_time', '>=', strtotime($start_date)*1000)
+            ->where('submit_time', '<', strtotime("tomorrow")*1000)
+            ->where('clevel', 'like', '%c3b%')
+            ->orderBy('submit_time', 'desc')->get();
 
-        $mol_diff = array_diff($arr_phone_mol, $arr_phone_helios);
-        $helios_diff = array_diff($arr_phone_helios, $arr_phone_mol);
+        $mol_contacts       = $this->getMOLContacts($start_date, $end_date);
 
-        foreach($helios_contacts as $key => $contact){
-            if(!in_array('0'.$contact->phone, $helios_diff)){
-                unset($helios_contacts[$key]);
+        $mol_diff       = $this->diff_contact_mol($helios_contacts, $mol_contacts);
+        $helios_diff    = $this->diff_contact_helios($helios_contacts, $mol_contacts);
+
+        return array($helios_diff, $mol_diff);
+
+    }
+
+
+    // mol co, helios ko
+    private function diff_contact_mol($helios_contacts, $mol_contacts){
+
+        foreach($helios_contacts as $helios){
+
+            foreach($mol_contacts as $key => $mol){
+                if (strpos($mol['phone'], $helios->phone) !== false) {
+                    unset($mol_contacts[$key]);
+                    continue;
+                }
+                $date = date_create($mol['datetime_submitted']);
+                $mol['datetime_submitted'] = date_format($date,"d-m-Y H:i:s");
             }
         }
 
-        foreach($mol_contacts as $key => $contact){
-            if(!in_array($contact['phone'], $mol_diff)){
-                unset($mol_contacts[$key]);
+        return $mol_contacts;
+
+    }
+
+    // helios co, mol ko
+    private function diff_contact_helios($helios_contacts, $mol_contacts){
+
+        foreach($mol_contacts as $mol){
+
+            foreach($helios_contacts as $key => $helios) {
+                if (strpos($mol['phone'], $helios->phone) !== false) {
+                    unset($helios_contacts[$key]);
+                    continue;
+                }
             }
         }
 
-        return array($helios_contacts, $mol_contacts);
+        return $helios_contacts;
+
+    }
+
+    public function filter(){
+
+        $start_date = \request('start_date');
+        $end_date   = \request('end_date');
+
+        list($helios_diff, $mol_diff) = $this->diff_contact($start_date, $end_date);
+        return view('pages.table_diff_contacts', compact(
+            'helios_diff',
+            'mol_diff'
+        ));
 
     }
 
--- resources/views/components/script-diff_contacts.blade.php
+++ resources/views/components/script-diff_contacts.blade.php
@@ -1,11 +1,58 @@
 <script>
+    $(document).ready(function () {
+        inittable($('#table_mol_helios'));
+        inittable($('#table_helios_mol'));
+    })
+
+    function inittable(table){
+        var page_size   = $('input[name="page_size"]').val();
+        /* BASIC ;*/
+        var responsiveHelper_table_channel = undefined;
+
+        var breakpointDefinition = {
+            tablet: 1024,
+            phone: 480
+        };
+
+        table.dataTable({
+            "sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6'f><'col-sm-6 col-xs-6 hidden-xs'C>r>" +
+            "t" +
+            "<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-sm-6 col-xs-12'p>>",
+            "autoWidth": true,
+            "preDrawCallback": function () {
+                // Initialize the responsive datatables helper once.
+                if (!responsiveHelper_table_channel) {
+                    responsiveHelper_table_channel = new ResponsiveDatatablesHelper(table, breakpointDefinition);
+                }
+            },
+            "rowCallback": function (nRow) {
+                responsiveHelper_table_channel.createExpandIcon(nRow);
+            },
+            "drawCallback": function (oSettings) {
+                responsiveHelper_table_channel.respond();
+            },
+            "order": [[4, "desc"]],
+            "iDisplayLength": parseInt(page_size),
+            'scrollY'       : '55vh',
+            "scrollX"       : true,
+            'scrollCollapse': true,
+            "destroy": true,
+        });
+    }
+
     $(function(){
 
         var start = moment();
         var end = moment();
 
         function reportrange_span(start, end) {
-            $('#reportrange span').html(start.format('D/M/Y') + '-' + end.format('D/M/Y'));
+            var init_date = new Date('6/17/2018');
+
+            if(start < init_date){
+                $('#reportrange span').html('17/6/2018' + '-' + end.format('D/M/Y'));
+            }else{
+                $('#reportrange span').html(start.format('D/M/Y') + '-' + end.format('D/M/Y'));
+            }
         }
 
         reportrange_span(start, end);
@@ -26,27 +73,30 @@
             }
         }, reportrange_span);
 
-        function c3range_span(start, end) {
-            $('#c3range span').html(start.format('D/M/Y') + '-' + end.format('D/M/Y'));
-        }
+        $('#reportrange').on('hide.daterangepicker', function(ev, picker) {
 
-        c3range_span(start, end);
+            var url         = $('input[name=filter_url]').val();
+            $('.loading').show();
+            $.ajax({
+                url: url,
+                type: 'GET',
+                data: {
+                    registered_date : $('.registered_date').text()
+                }
+            }).done(function (response) {
+                $('.wrapper_table').html(response);
+                inittable($('#table_mol_helios'));
+                inittable($('#table_helios_mol'));
+                console.log(response);
+            });
+            setTimeout(function(){
+                // initDataTable();
+                $('.loading').hide();
+            }, 1000);
 
-        $('#c3range').daterangepicker({
-            startDate: start,
-            endDate: end,
-            opens: 'right',
-            ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                "This Week":[moment().startOf("isoWeek"),moment().endOf("isoWeek")],
-                "Last Week": [moment().subtract(1, "week").startOf("isoWeek"), moment().subtract(1, "week").endOf("isoWeek")],
-                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            }
-        }, c3range_span);
+
+
+        });
 
     });
 
