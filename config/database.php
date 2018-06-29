185e52912d47e1240efd51f8dbdb1b09e8a246ff
Thanh Lam <lamvt@topica.edu.vn>
2018-06-29 16:50:43 +0700

    revert code

--- config/database.php
+++ config/database.php
@@ -80,9 +80,16 @@ return [
 
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
+            'host'     => ['db01','db02','db03'],
             'port'     => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE'),
+	    'username' => env('DB_USERNAME'),
+	    'password' => env('DB_PASSWORD'),
+	    'options'  => [
+		'replicaSet' => 'heliosrepl',
+		'database' => 'helios'
+		]
+
         ],
 
     ],
