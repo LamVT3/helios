c3c467ef3deee8a2aa813eaa93e0a0b06f1e0e60
Thanh Lam <lamvt@topica.edu.vn>
2018-07-11 17:03:48 +0700

    add choose currency for Dashboard, report quality

---
+++ resources/views/components/currency.blade.php
@@ -0,0 +1,15 @@
+<div id="lst_currency" class="col-sm-8 pull-right">
+    <label class="radio-inline radio_currency">
+        <input type="radio" name="currency" id="currency" value="USD" checked>USD
+        <span class="checkmark"></span>
+    </label>
+    <label class="radio-inline radio_currency">
+        <input type="radio" name="currency" id="currency" value="VND">VND
+        <span class="checkmark"></span>
+    </label>
+    <label class="radio-inline radio_currency">
+        <input type="radio" name="currency" id="currency" value="Baht">Baht
+        <span class="checkmark"></span>
+    </label>
+</div>
+<input type="hidden" id="currency_unit" value="USD">
