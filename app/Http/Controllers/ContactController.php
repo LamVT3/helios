5071d388c9bcb7006da8414b9353c021c9a2c937
Thanh Lam <lamvt@topica.edu.vn>
2018-09-07 12:51:27 +0700

    update uuid

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -914,17 +914,10 @@ class ContactController extends Controller
 
     private function gen_contact_id()
     {
-        $uuid = $this->uuid();
-        $contact_id = (string)$uuid + 'TL';
-
-        return $contact_id;
-    }
-
-    private function uuid(){
         $data = random_bytes(16);
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+        return vsprintf('%s%s-%s-%s-%s-%s%s%sTL', str_split(bin2hex($data), 4));
     }
 
     private function validate_c3a($contact){
