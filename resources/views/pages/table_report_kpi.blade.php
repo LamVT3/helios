3c6b2fbeb1d2aeef7c6aa0f8cbcc1a32fcfc43c7
Thien LH <hongthien0110@gmail.com>
2018-10-08 18:29:44 +0700

    ### - improve assign kpi for user, channel

--- resources/views/pages/table_report_kpi.blade.php
+++ resources/views/pages/table_report_kpi.blade.php
@@ -34,24 +34,24 @@
                {{--data-original-title='Edit Row'><i class='fa fa-pencil'></i></a>--}}
         </td>
         <?php
-            $total_kpi = $data_maketer['total']['total_kpi'];
-            $total_actual = $data_maketer['total']['total_actual'];
-            $total_gap = $kpi_selection == "c3b_cost" ? ($total_kpi - $total_actual) : ($total_actual - $total_kpi) ;
+            $total_kpi = isset($data_maketer['total']['total_kpi']) ? $data_maketer['total']['total_kpi'] : 0;
+            $total_actual = isset($data_maketer['total']['total_actual']) ? $data_maketer['total']['total_actual'] : 0;
+            $total_gap = @$kpi_selection == "c3b_cost" ? ($total_kpi - $total_actual) : ($total_actual - $total_kpi) ;
         ?>
-        <td class="border-bold-right total_text">{{ $total_kpi.($kpi_selection == 'l3_c3bg' ? '%' : '') }}</td>
-        <td class="border-bold-right total_text">{{ $total_actual.($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
-        <td class="border-bold-right total_text">{{ $total_gap.($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+        <td class="border-bold-right total_text">{{ $total_kpi.(@$kpi_selection == 'l3_c3bg' ? '%' : '') }}</td>
+        <td class="border-bold-right total_text">{{ $total_actual.(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+        <td class="border-bold-right total_text">{{ $total_gap.(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
 
         @for ($i = 1; $i <= $days; $i++)
-            <td>{{ $data_maketer['total']['kpi'][$i].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
-            <td class="border-bold-right act">{{ $data_maketer['total']['actual'][$i].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+            <td>{{ (isset($data_maketer['total']['kpi'][$i]) ? $data_maketer['total']['kpi'][$i] : 0).(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+            <td class="border-bold-right act">{{ (isset($data_maketer['total']['actual'][$i]) ? $data_maketer['total']['actual'][$i] : 0).(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
         @endfor
     </tr>
     @endif
     @foreach($data_maketer as $user => $item)
         @if($user == "total") @continue @endif
         <tr data-tt-id="{{@$item['user_id']}}">
-            <?php $gap = $kpi_selection == "c3b_cost" ? @$item['total_kpi'] - @$item['total_actual'] : @$item['total_actual'] - @$item['total_kpi'] ?>
+            <?php $gap = @$kpi_selection == "c3b_cost" ? @$item['total_kpi'] - @$item['total_actual'] : @$item['total_actual'] - @$item['total_kpi'] ?>
             <td class="no-border-right" style="border-left: none">
                 <a href="javascript:void(0)" title="Show detail" class="channel__show"><i class="fa fa-plus-circle"></i></a>
                 <a href="javascript:void(0)" title="Hide detail" class="channel__hide hidden"><i class="fa fa-minus-circle"></i></a>
@@ -68,25 +68,25 @@
                             class='fa fa-pencil'></i></a>
             </td>
 
-            <td class="border-bold-right total_text">{{ @$item['total_kpi'].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+            <td class="border-bold-right total_text">{{ @$item['total_kpi'].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
             @if($gap < 0)
-                <td class="border-bold-right total_text gap_text">{{ @$item['total_actual'].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
-                <td class="border-bold-right total_text gap_text">{{ $gap.($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                <td class="border-bold-right total_text gap_text">{{ @$item['total_actual'].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                <td class="border-bold-right total_text gap_text">{{ $gap.(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
             @else
-                <td class="border-bold-right total_text">{{ @$item['total_actual'].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
-                <td class="border-bold-right total_text">{{ $gap.($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                <td class="border-bold-right total_text">{{ @$item['total_actual'].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                <td class="border-bold-right total_text">{{ $gap.(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
             @endif
 
             @for ($i = 1; $i <= $days; $i++)
-                <?php $gap_day = $kpi_selection == "c3b_cost" ? @$item['kpi'][$i] - @$item['actual'][$i] : @$item['actual'][$i] - @$item['kpi'][$i] ?>
-                <td>{{ (@$item['kpi'][$i] ? @$item['kpi'][$i] : 0).($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                <?php $gap_day = @$kpi_selection == "c3b_cost" ? @$item['kpi'][$i] - @$item['actual'][$i] : @$item['actual'][$i] - @$item['kpi'][$i] ?>
+                <td>{{ (@$item['kpi'][$i] ? @$item['kpi'][$i] : 0).(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
                 @if(@$gap_day < 0)
                     <td class="border-bold-right act gap_text ">
-                        {{ (@$item['actual'][$i] ? @$item['actual'][$i] : 0).($kpi_selection == 'l3_c3bg' ? '%' : '')  }}
+                        {{ (@$item['actual'][$i] ? @$item['actual'][$i] : 0).(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}
                     </td>
                 @else
                     <td class="border-bold-right ">
-                        {{ (@$item['actual'][$i] ? @$item['actual'][$i] : 0).($kpi_selection == 'l3_c3bg' ? '%' : '')  }}
+                        {{ (@$item['actual'][$i] ? @$item['actual'][$i] : 0).(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}
                     </td>
                 @endif
             @endfor
@@ -98,13 +98,13 @@
                     <td class="no-border-right" style="border-left: none"></td>
                     <td class="no-border-right" style="text-align: left"><span>{{$key}}</span></td>
                     <td class="border-bold-right"></td>
-                    <td class="border-bold-right total_text">{{ $value['total_kpi'].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                    <td class="border-bold-right total_text">{{ $value['total_kpi'].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
                     @if($gap < 0)
-                        <td class="border-bold-right total_text gap_text">{{ $value['total_actual'].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
-                        <td class="border-bold-right total_text gap_text">{{ $gap.($kpi_selection == 'l3_c3bg' ? '%' : '') }}</td>
+                        <td class="border-bold-right total_text gap_text">{{ $value['total_actual'].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                        <td class="border-bold-right total_text gap_text">{{ $gap.(@$kpi_selection == 'l3_c3bg' ? '%' : '') }}</td>
                     @else
-                        <td class="border-bold-right total_text">{{ $value['total_actual'].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
-                        <td class="border-bold-right total_text">{{ $gap.($kpi_selection == 'l3_c3bg' ? '%' : '') }}</td>
+                        <td class="border-bold-right total_text">{{ $value['total_actual'].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                        <td class="border-bold-right total_text">{{ $gap.(@$kpi_selection == 'l3_c3bg' ? '%' : '') }}</td>
                     @endif
 
                     @for ($i = 1; $i <= $days; $i++)
@@ -115,11 +115,11 @@
                                 ($value['kpi'][$i] - $value['actual'][$i]) :
                                 ($value['actual'][$i] - $value['kpi'][$i]);
                         ?>
-                        <td>{{ $value['kpi'][$i].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                        <td>{{ $value['kpi'][$i].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
                         @if($gap_day < 0)
-                            <td class="border-bold-right gap_text">{{ $value['actual'][$i].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                            <td class="border-bold-right gap_text">{{ $value['actual'][$i].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
                         @else
-                            <td class="border-bold-right">{{ $value['actual'][$i].($kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
+                            <td class="border-bold-right">{{ $value['actual'][$i].(@$kpi_selection == 'l3_c3bg' ? '%' : '')  }}</td>
                         @endif
                     @endfor
                 </tr>
