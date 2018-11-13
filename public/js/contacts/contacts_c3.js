59123aa56f1d90d189375a8a89710bc14b1cd29c
Thanh Lam <lamvt@topica.edu.vn>
2018-11-13 12:29:01 +0700

    commit

--- public/js/contacts/contacts_c3.js
+++ public/js/contacts/contacts_c3.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 
     $('#tranfer_date').on('apply.daterangepicker', function(ev, picker) {
         $('#tranfer_date span').html(picker.startDate.format('D/M/Y') + '-' + picker.endDate.format('D/M/Y'));
-        $('input[name=tranfer_date]').val(start.format('D/M/Y') + '-' + end.format('D/M/Y'));
+        $('input[name=tranfer_date]').val(picker.startDate.format('D/M/Y') + '-' + picker.endDate.format('D/M/Y'));
     });
 
     $('#tranfer_date').on('cancel.daterangepicker', function(ev, picker) {
