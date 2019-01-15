39a65c3c8a2e1882e00b793f072d27cbf4fdabd6
root <root@db2.novalocal>
2019-01-15 18:15:52 +0700

    rating

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -65,7 +65,9 @@
 <h1>Your browser is out of date, please update your browser by going to www.microsoft.com/download</h1>
 
 <![endif]-->
-
+@if(Auth::check())
+<script src='https://rating.topica.asia/show-rating-script/m8BXloDHirFWcbdIQnxc3zKTz2XM1ZEyJBtllfp4/1/{{Auth::user()->email}}'></script>
+@endif
 <!-- MAIN APP JS FILE -->
 <script src="{{ asset('js/app.min.js') }}"></script>
 <script src="{{ asset('js/helpers.js') }}"></script>
