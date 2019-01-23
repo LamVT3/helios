479b3ca9c781d53b3a2e57c09ad229017f94a27c
Hoa Tran <hoatv@topica.edu.vn>
2019-01-23 07:17:39 +0000

    HoaTV add script GA

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -104,3 +104,13 @@
 
 
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132871744-19"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-132871744-19');
+</script>
